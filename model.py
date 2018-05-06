e2801ea5752625ab087d578e8b59110322e9a09c
Nazareno Castro <nazarenomartincastro@gmail.com>
2018-05-06 13:48:56 -0300

    Reservation per Hour test passed

--- model.py
+++ model.py
@@ -1,2 +1,19 @@
+import datetime
 
+
+COST_PER_HOUR = 5
+
+
+class ReservationPerHour():
+	""" A reservation that calculates its price by hour """
+	def __init__(self, start_date):
+		self.start_date = start_date
 		
+	
+	def end(self, end_date):
+		self.end_date = end_date
+
+	def final_price(self):
+		delta = self.end_date - self.start_date
+		hours = delta.days * 24 + delta.seconds / 3600.0
+		return hours * COST_PER_HOUR
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import unittest
-from datetime import date
+from datetime import datetime, timedelta
 from model import (
     ReservationPerHour, 
-    ReservationPerDay, 
-    ReservationPerWeek, 
-    COST_PER_DAY, 
+    # ReservationPerDay, 
+    # ReservationPerWeek, 
+    # COST_PER_DAY, 
     COST_PER_HOUR, 
-    COST_PER_WEEK, 
-    FAMILY_DISCOUNT
+    # COST_PER_WEEK, 
+    # FAMILY_DISCOUNT
 )
 
 # Context
@@ -38,7 +38,7 @@ class TestReservation(unittest.TestCase):
         start_date = datetime.now()
         reservation = ReservationPerHour(start_date)
         reserved_hours = 3
-        end_date = start_date + datetime.timedelta(hours=reserved_hours)
+        end_date = start_date + timedelta(hours=reserved_hours)
         reservation.end(end_date)
         self.assertEqual(
             COST_PER_HOUR * reserved_hours, 
@@ -46,98 +46,98 @@ class TestReservation(unittest.TestCase):
         )
 
 
-    def test_when_rent_a_bike_per_day_the_price_is_per_each_day(self):
-        start_date = datetime.now()
-        reservation = ReservationPerDay(start_date)
-        start_date = date.today()
-        reserved_days = 5
-        end_date = start_date + datetime.timedelta(days=reserved_days)
-        reservation.end(end_date)
-        self.assertEqual(
-            COST_PER_DAY * reserved_days, 
-            reservation.final_price()
-        )
+    # def test_when_rent_a_bike_per_day_the_price_is_per_each_day(self):
+    #     start_date = datetime.now()
+    #     reservation = ReservationPerDay(start_date)
+    #     start_date = date.today()
+    #     reserved_days = 5
+    #     end_date = start_date + datetime.timedelta(days=reserved_days)
+    #     reservation.end(end_date)
+    #     self.assertEqual(
+    #         COST_PER_DAY * reserved_days, 
+    #         reservation.final_price()
+    #     )
 
 
-    def test_when_rent_a_bike_per_week_the_price_is_per_each_week(self):
-        start_date = datetime.now()
-        reservation = ReservationPerWeek(start_date)
-        start_date = date.today()
-        reserved_days = 7
-        end_date = start_date + datetime.timedelta(days=reserved_days)
-        reservation.end(end_date)
-        self.assertEqual(
-            COST_PER_WEEK * reserved_days, 
-            reservation.final_price()
-        )
+    # def test_when_rent_a_bike_per_week_the_price_is_per_each_week(self):
+    #     start_date = datetime.now()
+    #     reservation = ReservationPerWeek(start_date)
+    #     start_date = date.today()
+    #     reserved_days = 7
+    #     end_date = start_date + datetime.timedelta(days=reserved_days)
+    #     reservation.end(end_date)
+    #     self.assertEqual(
+    #         COST_PER_WEEK * reserved_days, 
+    #         reservation.final_price()
+    #     )
 
 
-    def test_when_rent_type_family_the_price_is_the_total_with_discount(self):
-        start_date = datetime.now()
+    # def test_when_rent_type_family_the_price_is_the_total_with_discount(self):
+    #     start_date = datetime.now()
 
-        reservation_hour = ReservationPerHour(start_date)
-        reservation_day = ReservationPerDay(start_date)
-        reservation_week = ReservationPerWeek(start_date)
+    #     reservation_hour = ReservationPerHour(start_date)
+    #     reservation_day = ReservationPerDay(start_date)
+    #     reservation_week = ReservationPerWeek(start_date)
         
-        family_reservation = FamilyReservation(
-            [reservation_hour, reservation_day, reservation_week]
-        )
+    #     family_reservation = FamilyReservation(
+    #         [reservation_hour, reservation_day, reservation_week]
+    #     )
 
-        reservation_hour.end(start_date + datetime.timedelta(hours=12))
-        reservation_day.end(start_date + datetime.timedelta(days=5))
-        reservation_week.end(start_date + datetime.timedelta(days=24))
+    #     reservation_hour.end(start_date + datetime.timedelta(hours=12))
+    #     reservation_day.end(start_date + datetime.timedelta(days=5))
+    #     reservation_week.end(start_date + datetime.timedelta(days=24))
 
-        not_discounted_price = 
-            (COST_PER_DAY * 5) + 
-            (COST_PER_WEEK * 4) + 
-            (COST_PER_HOUR * 12)
+    #     not_discounted_price = 
+    #         (COST_PER_DAY * 5) + 
+    #         (COST_PER_WEEK * 4) + 
+    #         (COST_PER_HOUR * 12)
 
-        discount = (FAMILY_DISCOUNT * not_discounted_price) / 100.0
-        expected_cost = not_discounted_price - discount
+    #     discount = (FAMILY_DISCOUNT * not_discounted_price) / 100.0
+    #     expected_cost = not_discounted_price - discount
         
 
-        self.assertEqual(
-            expected_cost,
-            family_reservation.final_price()
-        )
+    #     self.assertEqual(
+    #         expected_cost,
+    #         family_reservation.final_price()
+    #     )
 
 
-    def test_family_reservation_cannot_have\
-        _more_than_five_asociated_rentals(self):
-        start_date = datetime.now()
+    # def test_family_reservation_cannot_have\
+    #     _more_than_five_asociated_rentals(self):
+    #     start_date = datetime.now()
 
-        reservation_list = [
-            ReservationPerHour(start_date) 
-            ,ReservationPerDay(start_date)
-            ,ReservationPerDay(start_date)
-            ,ReservationPerWeek(start_date)
-            ,ReservationPerWeek(start_date)
-            ,ReservationPerWeek(start_date)
-        ]
+    #     reservation_list = [
+    #         ReservationPerHour(start_date) 
+    #         ,ReservationPerDay(start_date)
+    #         ,ReservationPerDay(start_date)
+    #         ,ReservationPerWeek(start_date)
+    #         ,ReservationPerWeek(start_date)
+    #         ,ReservationPerWeek(start_date)
+    #     ]
 
-        with self.assertRaises(InvalidAmountOfReservationsOnFamiliyError):
-            family_reservation = FamilyReservation(reservation_list)
-
-
-    def test_family_reservation_cannot_have_\
-        less_than_three_asociated_rentals(self):
-        start_date = datetime.now()
-
-        reservation_list = [
-            ReservationPerHour(start_date) 
-            ,ReservationPerDay(start_date)
-        ]
-
-        with self.assertRaises(InvalidAmountOfReservationsOnFamiliyError):
-            family_reservation = FamilyReservation(reservation_list)
+    #     with self.assertRaises(InvalidAmountOfReservationsOnFamiliyError):
+    #         family_reservation = FamilyReservation(reservation_list)
 
 
-    def test_cannot_have_final_price_of_not_ended_reservation(self)
-        start_date = datetime.now()
-        reservation = ReservationPerHour(start_date) 
+    # def test_family_reservation_cannot_have_\
+    #     less_than_three_asociated_rentals(self):
+    #     start_date = datetime.now()
 
-        with self.assertRaises(ReservationNotEndedError):
-            reservation.final_price()
+    #     reservation_list = [
+    #         ReservationPerHour(start_date) 
+    #         ,ReservationPerDay(start_date)
+    #     ]
+
+    #     with self.assertRaises(InvalidAmountOfReservationsOnFamiliyError):
+    #         family_reservation = FamilyReservation(reservation_list)
+
+
+    # def test_cannot_have_final_price_of_not_ended_reservation(self)
+    #     start_date = datetime.now()
+    #     reservation = ReservationPerHour(start_date) 
+
+    #     with self.assertRaises(ReservationNotEndedError):
+    #         reservation.final_price()
 
 
 if __name__ == '__main__':
